1509550ac8c39e328313c6a62111422c6398499d
mihailo89 <mihailostefanovic89@gmail.com>
2021-04-02 14:47:23 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://github.com/mihailo89/"
           rel="noopener noreferrer"
         >
-          Learn React
+          Todo app in progress...
         </a>
       </header>
     </div>
